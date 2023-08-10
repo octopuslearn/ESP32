43594564ae6b0d4a52c9433420e00f64f745670d
octopuslearn <3148262242@qq.com>
2023-08-10 10:41:46 +0800

    日志 log

--- idf_esp_32/project/test_1.0/main/hello_world_main.c
+++ idf_esp_32/project/test_1.0/main/hello_world_main.c
@@ -5,10 +5,30 @@
  */
 
 #include <stdio.h>
+#include "esp_log.h"
 
 void app_main(void)
 {
-    printf("Hello world!");//没有\n啥也打印不出来,可是实测可以打印出来
-    printf("sss");
+    //E error
+    ESP_LOGE("error","不亮了");
+    //W warning
+    ESP_LOGW("waring","还能活两天");
+    //I information
+    ESP_LOGI("information","还能活一天");
+
+
+
+    //D Debug
+    ESP_LOGD("Debug","过度使用10000小时");
+    //V verbose
+    ESP_LOGV("verbose","损毁严重");
+/*
+    //N no log output
+    // ESP_LOGN("no log output","错误使用");//写ESP_LOGN就报错
+//    26 |     ESP_LOGN("no log output","閿欒浣跨敤");
+//       |     ^~~~~~~~
+//       |     ESP_LOGD
+*/
+
     fflush(stdout);//将标准输出冲马桶
 }
