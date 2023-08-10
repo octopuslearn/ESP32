edcaae6caeb9a3fca28fb354b9d24d770e368533
octopuslearn <3148262242@qq.com>
2023-08-10 22:22:33 +0800

    自定义menuconfig

--- idf_esp_32/project/test_1.0/main/hello_world_main.c
+++ idf_esp_32/project/test_1.0/main/hello_world_main.c
@@ -5,8 +5,8 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 
-#define LED_PIN GPIO_NUM_2//define的方式不占用内存
-#define DELAY_MS 2000
+#define LED_PIN CONFIG_LED_GPIO_NUM//define的方式不占用内存
+#define DELAY_MS CONFIG_DELAY_TIME_MS
 
 uint32_t staus=0;
 
@@ -14,6 +14,10 @@ void app_main(void)
 {  
     gpio_reset_pin(LED_PIN);//重置引脚2
     gpio_set_direction(LED_PIN,GPIO_MODE_OUTPUT);
+
+    ESP_LOGI("blink","GPIO : %d",LED_PIN);
+    ESP_LOGI("blink","DELAY : %d",DELAY_MS);
+
     while(1)
     {
         staus =! staus; 
