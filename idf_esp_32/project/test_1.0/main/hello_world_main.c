1a65ca5ace48d412b50d97337b52c1f016c5c677
octopuslearn <3148262242@qq.com>
2023-08-10 12:36:38 +0800

    大循环完 可更改tick的频率，从而改变延时时间
        注意此处：ESP_LOGI("COUNTER","Tick(ms) : %ld",portTICK_PERIOD_MS);

--- idf_esp_32/project/test_1.0/main/hello_world_main.c
+++ idf_esp_32/project/test_1.0/main/hello_world_main.c
@@ -1,50 +1,26 @@
-/*
- * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: CC0-1.0
- */
 
 #include <stdio.h>
 #include "esp_log.h"
-#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-const char* tag="绒布球";
-
-bool secretLog = true;
-
+int count = 0;
 void app_main(void)
-{
-
-if(secretLog == true)
-{
-    esp_log_level_set(tag, ESP_LOG_VERBOSE);//当按键被按下，则设置为输出详细信息
-}
-
-
-/*E,W,I是给用户看的*/
-    //E error
-    ESP_LOGE(tag,"眼睛失去高光");
-    //W warning
-    // ESP_LOGW(tag,"寿命剩余50%%");//需要两个%，否则会报错
-    ESP_LOGW(tag,"寿命剩余%i%%",50);//可以把数字弄出来
-    //I information
-    ESP_LOGI(tag,"寿命剩余10%%");
+{  
+    count = 100;
+    ESP_LOGI("COUNTER","Tick(ms) : %ld",portTICK_PERIOD_MS);
+    while(1)
+    {
+        ESP_LOGI("COUNTER","Count Value is %d", count);
+        count++;
+        //delay(1000);
+        // vTaskDelay(1000);//1000 ticks . 1ticks ???? ms--->1ticks-10ms
+        //I (10328) COUNTER: Count Value is 指开机后时间-ms,即10s
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
 
 
-
-
-/*D和V是给程序员看的，所以一般选的INFO*/
-    //D Debug
-    ESP_LOGD(tag,"过度使用10000小时");
-    //V verbose
-    ESP_LOGV(tag,"损毁严重");
-/*//因为是指啥都不显示
-    //N no log output
-    // ESP_LOGN("no log output","错误使用");//写ESP_LOGN就报错
-//    26 |     ESP_LOGN("no log output","閿欒浣跨敤");
-//       |     ^~~~~~~~
-//       |     ESP_LOGD
-*/
-
-    fflush(stdout);//将标准输出冲马桶
+    // for(;;)
+    // {
+    // }
 }
