bfbb9d5dec6dbfd1af0497c05e8e6bba8e3c2250
octopuslearn <3148262242@qq.com>
2023-08-10 10:54:52 +0800

    /*E,W,I是给用户看的*/  /*D和V是给程序员看的，所以一般选的INFO*/

--- idf_esp_32/project/test_1.0/main/hello_world_main.c
+++ idf_esp_32/project/test_1.0/main/hello_world_main.c
@@ -7,22 +7,29 @@
 #include <stdio.h>
 #include "esp_log.h"
 
+const char* tag="绒布球";
+
 void app_main(void)
 {
+
+/*E,W,I是给用户看的*/
     //E error
-    ESP_LOGE("error","不亮了");
+    ESP_LOGE(tag,"眼睛失去高光");
     //W warning
-    ESP_LOGW("waring","还能活两天");
+    // ESP_LOGW(tag,"寿命剩余50%%");//需要两个%，否则会报错
+    ESP_LOGW(tag,"寿命剩余%i%%",50);//可以把数字弄出来
     //I information
-    ESP_LOGI("information","还能活一天");
+    ESP_LOGI(tag,"寿命剩余10%%");
 
 
 
+
+/*D和V是给程序员看的，所以一般选的INFO*/
     //D Debug
-    ESP_LOGD("Debug","过度使用10000小时");
+    ESP_LOGD(tag,"过度使用10000小时");
     //V verbose
-    ESP_LOGV("verbose","损毁严重");
-/*
+    ESP_LOGV(tag,"损毁严重");
+/*//因为是指啥都不显示
     //N no log output
     // ESP_LOGN("no log output","错误使用");//写ESP_LOGN就报错
 //    26 |     ESP_LOGN("no log output","閿欒浣跨敤");
