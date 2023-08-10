b4105809685972f44cef946b12af8d091e170b23
octopuslearn <3148262242@qq.com>
2023-08-10 21:40:47 +0800

    使用define不使用内存

--- idf_esp_32/project/test_1.0/main/hello_world_main.c
+++ idf_esp_32/project/test_1.0/main/hello_world_main.c
@@ -3,35 +3,21 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/gpio.h"
 
-int count = 0;
-char ptrTaskList[250];
+#define LED_PIN GPIO_NUM_2//define的方式不占用内存
+#define DELAY_MS 2000
 
+uint32_t staus=0;
 
 void app_main(void)
 {  
-    /*不知道上哪去设置，v5.1没找到，找到了*/
-    vTaskList(ptrTaskList);
-    printf("***********************\n");
-    printf("Task        State       Prio    Stack   Num\n");
-    printf("***********************\n");
-    printf(ptrTaskList);
-    printf("***********************\n");
-    /*不知道上哪去设置，v5.1没找到*/
-//5.1需要找到FreeRTOS>Kernel>configUSE_TRACE_FACILITY（打勾）>Enable display of xCoreID in vTaskList（打勾）
-    ESP_LOGI("COUNTER","Tick(ms) : %ld",portTICK_PERIOD_MS);
+    gpio_reset_pin(LED_PIN);//重置引脚2
+    gpio_set_direction(LED_PIN,GPIO_MODE_OUTPUT);
     while(1)
     {
-        ESP_LOGI("COUNTER","Count Value is %d", count);
-        count++;
-        //delay(1000);
-        // vTaskDelay(1000);//1000 ticks . 1ticks ???? ms--->1ticks-10ms
-        //I (10328) COUNTER: Count Value is 指开机后时间-ms,即10s
-        vTaskDelay(1000/portTICK_PERIOD_MS);
+        staus =! staus; 
+        gpio_set_level(LED_PIN,staus);
+        vTaskDelay(DELAY_MS/portTICK_PERIOD_MS);
     }
-
-
-    // for(;;)
-    // {
-    // }
 }
