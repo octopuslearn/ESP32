a57210b61ae090bba50ef0fe49d909e4787a550c
octopuslearn <3148262242@qq.com>
2023-08-11 19:37:12 +0800

    不知道如何更改波特率
    I (1354) NVS: ssid:password ����������������������������L�?:������#

--- idf_esp_32/project/test_1.0/main/hello_world_main.c
+++ idf_esp_32/project/test_1.0/main/hello_world_main.c
@@ -4,6 +4,7 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include <string.h>
 
 
 
@@ -11,24 +12,42 @@
 void app_main(void)
 {
     vTaskDelay(1000/portTICK_PERIOD_MS);//I (1366) NVS: KEY:VALUE counter:0 
-    char* ocLearn_name_space = "ocLearn_1234";
+    char* ocLearn_name_space = "ocLearn_1234";////问题在这，更改了ocLearn_1234到ocLearn_1234567就会出现一直是0
     nvs_flash_init();//相当于把u盘插到电脑上
     nvs_handle_t ocLearn_handle;//句柄，所谓句柄就是文件夹的窗口
 
    
     nvs_open(ocLearn_name_space,NVS_READWRITE, &ocLearn_handle);//这一步相当于建立了个ocLearn_handle文件夹，并打开了他，他是可读可写的//NVS_READWRITE-给了读写权限
 
-    //read
-    //write
+uint32_t max_ap=2;
+//结构体
+typedef struct{
+    char ssid[50];
+    char password[50];
+} ap_t;
 
-    uint32_t counter_val = 0;
-    char* counter_key = "counter";
-    nvs_get_u32(ocLearn_handle, counter_key, &counter_val);//找文件夹里有没有counter文件,有的话计数
-    ESP_LOGI("NVS","KEY:VALUE %s:%ld ",counter_key,counter_val);
-    
-    counter_val++;
+ap_t aps_set[max_ap];//创建结构体的数组
 
-    nvs_set_u32(ocLearn_handle, counter_key, counter_val);//到文件夹里创建了一个新的文件counter文件，value是1
+for(int i=0;i<max_ap;i++)
+{
+    strcpy(aps_set[i].ssid,"ocLean");//c++中不让这么用aps_set[i].ssid="ocLean";而应该strcpy(aps_set[i].ssid,"ocLean");
+    strcpy(aps_set[i].password,"123456");
+    // aps_set[i].ssid="ocLean";//c++中不让这么用aps_set[i].ssid="ocLean";而应该strcpy(aps_set[i].ssid,"ocLean");
+}
+
+nvs_set_blob(ocLearn_name_space,"aps",aps_set,sizeof(aps_set));//向NVS中写入了复杂二进制数据
+
+
+ap_t aps_get[max_ap];//创建结构体的数组
+size_t length = sizeof(aps_get);
+nvs_get_blob(ocLearn_name_space,"aps",aps_get,&length);//向NVS中写入了复杂二进制数////文件夹-文件-内容-内容长度
+
+
+for(int i=0;i<max_ap;i++)
+{
+        ESP_LOGI("NVS","ssid:password  %s:%s",aps_get[i].ssid,aps_get[i].password);
+}
+
     nvs_commit(ocLearn_handle);//快速执行nvs_set_u32
 
     nvs_close(ocLearn_handle);
