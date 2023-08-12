5c3567bf84748815d32307439a6e435f4c34d765
octopuslearn <3148262242@qq.com>
2023-08-12 10:14:25 +0800

    ESP_ERR_NVS_INVALID_HANDLE句柄已关闭或为空-理由
    原因：句柄写错了
    
    现在已经改好了
        nvs_handle_t ocLearn_handle;//句柄，所谓句柄就是文件夹的窗口
        nvs_open(ocLearn_handle,NVS_READWRITE, &ocLearn_handle);//这一步相当于建立了个ocLearn_handle文件夹，并打开了他，他是可读可写的//NVS_READWRITE-给了读写权限

--- idf_esp_32/project/test_1.0/main/hello_world_main.c
+++ idf_esp_32/project/test_1.0/main/hello_world_main.c
@@ -5,6 +5,9 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include <string.h>
+#include "esp_random.h"////此处我查ESP API写的%lu
+#include "esp_err.h"//返回错误代码
+
 
 /*打印内存中的数据*/
 void printMemory(unsigned char *address, int size)
@@ -23,33 +26,30 @@ uint32_t num = 200;//在内存中打印出
 
 void app_main(void)
 {
-    vTaskDelay(1000/portTICK_PERIOD_MS);//I (1366) NVS: KEY:VALUE counter:0 
-
+    //vTaskDelay(1000/portTICK_PERIOD_MS);//I (1366) NVS: KEY:VALUE counter:0 
+    
     // printMemory((unsigned char *)&num,4);
     // num=100;
     // printMemory((unsigned char *)&num,4);
 
     char* ocLearn_name_space = "ocLearn_1234";////问题在这，更改了ocLearn_1234到ocLearn_1234567就会出现一直是0
-    nvs_flash_init();//相当于把u盘插到电脑上
+    esp_err_t err = nvs_flash_init();//相当于把u盘插到电脑上
+    ESP_ERROR_CHECK(err);//倘若，NVS初始化不成功，会用尝试重启的方式再次初始化
     nvs_handle_t ocLearn_handle;//句柄，所谓句柄就是文件夹的窗口
+    nvs_open(ocLearn_handle,NVS_READWRITE, &ocLearn_handle);//这一步相当于建立了个ocLearn_handle文件夹，并打开了他，他是可读可写的//NVS_READWRITE-给了读写权限
 
-   
-    nvs_open(ocLearn_name_space,NVS_READWRITE, &ocLearn_handle);//这一步相当于建立了个ocLearn_handle文件夹，并打开了他，他是可读可写的//NVS_READWRITE-给了读写权限
 
-uint32_t max_ap=2;
+
+uint32_t max_ap=20;
 //结构体
 typedef struct{
     char ssid[50];
     char password[50];
 } ap_t;
-
 ap_t aps_set[max_ap];//创建结构体的数组////声明
-
-//printMemory((unsigned char *)&aps_set,sizeof(aps_set));
-//只声明不初始化20000600204dfb3f00000000044cfb3f0c4cfb3f23000600ffff3fb3ffff3fb319000000204cfb3f204afb3f0200000020b10880604dfb3fe0370e40183f403fc04dfb3fa04dfb3f0c000000ffff3fb3a04dfb3f0c000000842afb3fffff3fb38cad0e80904dfb3f03000000183f403fc04dfb3fa04dfb3f0c00000010000000983f403fc04dfb3fa04dfb3f0c000000dc840880e04dfb3f183f403f01000000c04dfb3fa04dfb3f0c000000ffff3fb3dc840880e04dfb3f0300000051010000183f403f00000000
 memset(aps_set,0,sizeof(aps_set));//初始化//声明后必须初始化
-printMemory((unsigned char *)&aps_set,sizeof(aps_set));
-//声明初始化0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+
+
 
 for(int i=0;i<max_ap;i++)
 {
@@ -58,7 +58,21 @@ for(int i=0;i<max_ap;i++)
     // aps_set[i].ssid="ocLean";//c++中不让这么用aps_set[i].ssid="ocLean";而应该strcpy(aps_set[i].ssid,"ocLean");
 }
 
-nvs_set_blob(ocLearn_name_space,"aps",aps_set,sizeof(aps_set));//向NVS中写入了复杂二进制数据
+
+char key[15];
+sprintf(key,"key %lu", esp_random());////此处我查资料字节写的%lu
+esp_err_t erro = nvs_set_blob(ocLearn_handle,key,aps_set,sizeof(aps_set));//向NVS中写入了复杂二进制数据
+nvs_commit(ocLearn_handle);//快速执行nvs_set_u32
+if(erro ==  ESP_OK)
+{
+    esp_restart();//它会导致设备立即重新启动，从而重新加载固件和初始化
+}
+else
+{
+    ESP_LOGE("NVS", "nvs_set_blob erro:  %s", esp_err_to_name(erro));
+    // esp_err_to_name_r(erro);
+    // ESP_LOGI("NVS", "nvs_set_blob erro:  %d", erro);//ESP_ERR_NVS_INVALID_HANDLE (0x1107): Handle has been closed or is NUL
+}
 
 
 ap_t aps_get[max_ap];//创建结构体的数组
