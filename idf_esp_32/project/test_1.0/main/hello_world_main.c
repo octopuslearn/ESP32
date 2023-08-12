981517caed27ce5111f14b53a8014b8e8650af45
octopuslearn <3148262242@qq.com>
2023-08-12 10:42:50 +0800

    并未打印 不知道为啥

--- idf_esp_32/project/test_1.0/main/hello_world_main.c
+++ idf_esp_32/project/test_1.0/main/hello_world_main.c
@@ -34,56 +34,59 @@ void app_main(void)
 
     char* ocLearn_name_space = "ocLearn_1234";////问题在这，更改了ocLearn_1234到ocLearn_1234567就会出现一直是0
     esp_err_t err = nvs_flash_init();//相当于把u盘插到电脑上
-    ESP_ERROR_CHECK(err);//倘若，NVS初始化不成功，会用尝试重启的方式再次初始化
+
+    // ESP_ERROR_CHECK(nvs_flash_init());//倘若，NVS初始化不成功，会用尝试重启的方式再次初始化
+
     nvs_handle_t ocLearn_handle;//句柄，所谓句柄就是文件夹的窗口
     nvs_open(ocLearn_handle,NVS_READWRITE, &ocLearn_handle);//这一步相当于建立了个ocLearn_handle文件夹，并打开了他，他是可读可写的//NVS_READWRITE-给了读写权限
 
 
 
-uint32_t max_ap=20;
-//结构体
-typedef struct{
-    char ssid[50];
-    char password[50];
-} ap_t;
-ap_t aps_set[max_ap];//创建结构体的数组////声明
-memset(aps_set,0,sizeof(aps_set));//初始化//声明后必须初始化
+    uint32_t max_ap=20;
+    //结构体
+    typedef struct{
+        char ssid[50];
+        char password[50];
+    } ap_t;
+    ap_t aps_set[max_ap];//创建结构体的数组////声明
+    memset(aps_set,0,sizeof(aps_set));//初始化//声明后必须初始化
 
 
 
-for(int i=0;i<max_ap;i++)
-{
-    strcpy(aps_set[i].ssid,"ocLean");//c++中不让这么用aps_set[i].ssid="ocLean";而应该strcpy(aps_set[i].ssid,"ocLean");
-    strcpy(aps_set[i].password,"123456");
-    // aps_set[i].ssid="ocLean";//c++中不让这么用aps_set[i].ssid="ocLean";而应该strcpy(aps_set[i].ssid,"ocLean");
-}
+    for(int i=0;i<max_ap;i++)
+    {
+        strcpy(aps_set[i].ssid,"ocLean");//c++中不让这么用aps_set[i].ssid="ocLean";而应该strcpy(aps_set[i].ssid,"ocLean");
+        strcpy(aps_set[i].password,"123456");
+        // aps_set[i].ssid="ocLean";//c++中不让这么用aps_set[i].ssid="ocLean";而应该strcpy(aps_set[i].ssid,"ocLean");
+    }
 
 
-char key[15];
-sprintf(key,"key %lu", esp_random());////此处我查资料字节写的%lu
-esp_err_t erro = nvs_set_blob(ocLearn_handle,key,aps_set,sizeof(aps_set));//向NVS中写入了复杂二进制数据
-nvs_commit(ocLearn_handle);//快速执行nvs_set_u32
-if(erro ==  ESP_OK)
-{
-    esp_restart();//它会导致设备立即重新启动，从而重新加载固件和初始化
-}
-else
-{
-    ESP_LOGE("NVS", "nvs_set_blob erro:  %s", esp_err_to_name(erro));
-    // esp_err_to_name_r(erro);
-    // ESP_LOGI("NVS", "nvs_set_blob erro:  %d", erro);//ESP_ERR_NVS_INVALID_HANDLE (0x1107): Handle has been closed or is NUL
-}
+    char key[15];
+    sprintf(key,"key %lu", esp_random());////此处我查资料字节写的%lu
+    
+    ESP_ERROR_CHECK(nvs_set_blob(ocLearn_handle,key,aps_set,sizeof(aps_set)));//向NVS中写入了复杂二进制数据
+    
+    
+    nvs_commit(ocLearn_handle);//快速执行nvs_set_u32
+    // if(erro ==  ESP_OK)
+    // {
+    //     esp_restart();//它会导致设备立即重新启动，从而重新加载固件和初始化
+    // }
+    // else
+    // {
+    //     ESP_LOGE("NVS", "nvs_set_blob erro:  %s", esp_err_to_name(erro));
+    // }
 
 
-ap_t aps_get[max_ap];//创建结构体的数组
-size_t length = sizeof(aps_get);
-nvs_get_blob(ocLearn_name_space,"aps",aps_get,&length);//向NVS中写入了复杂二进制数////文件夹-文件-内容-内容长度
+    ap_t aps_get[max_ap];//创建结构体的数组
+    size_t length = sizeof(aps_get);
+    nvs_get_blob(ocLearn_name_space,"aps",aps_get,&length);//向NVS中写入了复杂二进制数////文件夹-文件-内容-内容长度
 
 
-for(int i=0;i<max_ap;i++)
-{
-        //ESP_LOGI("NVS","ssid:password  %s:%s",aps_get[i].ssid,aps_get[i].password);
-}
+    for(int i=0;i<max_ap;i++)
+    {
+            //ESP_LOGI("NVS","ssid:password  %s:%s",aps_get[i].ssid,aps_get[i].password);
+    }
 
     nvs_commit(ocLearn_handle);//快速执行nvs_set_u32
 
