282b60b38d1551ab5e1459833531f61c0b86eabd
octopuslearn <3148262242@qq.com>
2023-08-22 20:18:42 +0800

    xTaskCreate使用 动态方法创建一个任务
     vTaskDelay  freeRTOS延时函数，延时的是tick而不是ms
    对于esp32 一个tick是1ms

--- freeRTOS_arduino-esp32/freeRTOS_arduino-esp32.ino
+++ freeRTOS_arduino-esp32/freeRTOS_arduino-esp32.ino
@@ -1,19 +1,38 @@
-#define led1 10
-#define led2 11
+#define led1 18
+#define led2 19
+
+
+void task1(void *pt)//形参-空指针
+{
+    pinMode(led1, OUTPUT);
+    while(1)
+    {
+      digitalWrite(led1,!digitalRead(led1));
+      //delay(1000);//阻塞
+      vTaskDelay(2000);//freeRTOS的延时函数
+    }
+}
+
+
+void task2(void *pt)//形参-空指针
+{
+    pinMode(led2, OUTPUT);
+    while(1)
+    {
+      digitalWrite(led2,!digitalRead(led2));
+      vTaskDelay(1000);//freeRTOS的延时函数
+    }
+}
+
 
 //很明显，这样不能是的led1亮1s,led2亮2s。因为代码是一行行运行的，led1亮了4秒
 void setup() {
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
+  xTaskCreate(task1, "Blik 10", 1024, NULL, 1, NULL);//task-任务函数，"Blik 10-任务名词", 1024-任务堆栈大小给任务分配的空间，NULL-传递给任务的参数，1-任务优先级，NULL-任务句柄（创建、删除、挂起……）
+  xTaskCreate(task2, "Blik 11", 1024, NULL, 1, NULL);
   // put your setup code here, to run once:
 
 }
 
 void loop() {
-  digitalWrite(led1,!digitalRead(led1));
-  delay(1000);
-  digitalWrite(led2,!digitalRead(led2));
-  delay(3000);
-  
   // put your main code here, to run repeatedly:
 }
