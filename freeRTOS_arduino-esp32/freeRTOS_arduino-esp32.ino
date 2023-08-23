85be6e5727cbb63428b99117b2f2a7b6d6dbbc0a
octopuslearn <3148262242@qq.com>
2023-08-23 18:23:30 +0800

    在两个任务之间传递参数，可用全局变量。但是写的只能有一个，但是读的不限
    //                 1024*4,//问题在这将1024改成1024*4即可，问题是如何确定内存
    //while(1){};vTaskDelay(10);//???while循环还能加分号吗

--- freeRTOS_arduino-esp32/freeRTOS_arduino-esp32.ino
+++ freeRTOS_arduino-esp32/freeRTOS_arduino-esp32.ino
@@ -1,62 +1,75 @@
-byte LED1PIN = 18;
-byte LED2PIN = 19;
+//                 1024*4,//问题在这将1024改成1024*4即可，问题是如何确定内存
+//while(1){};vTaskDelay(10);//???while循环还能加分号吗
 
-typedef struct {//定义结构体
-  byte ledpin;//led引脚
-  int delayTime;//延时时间
-}LEDFLASH;
+
+
+volatile uint32_t inventory = 100;//总库存
+volatile uint32_t retailCount = 0;//线下销售量
 
 void task1(void *pt)//形参-空指针
 {
-  LEDFLASH *ptLedFlash = (LEDFLASH *)pt;
-  byte pin = ptLedFlash -> ledpin;
-  int delayTime = ptLedFlash -> delayTime;
-    pinMode(pin, OUTPUT);
     while(1)
     {
-      digitalWrite(pin,!digitalRead(pin));
-      vTaskDelay(delayTime/portTICK_PERIOD_MS);//portTICK_PERIOD_MS将ms单位转换成时钟节拍数
-    }
+      uint32_t inv = inventory;
+      for(int i; i < random(10, 100); i++)//random(10,100)-生成10-100随机数
+        vTaskDelay(pdMS_TO_TICKS(i));//将时间转换成节拍数
+      if(inventory > 0)//有库存
+      {
+        inventory = inv - 1;//库存--
+        retailCount++;//销售量++
+      }
+    };//???while循环还能加分号吗
+    vTaskDelay(10);//减慢速度，以待升级
 }
 
 void task2(void *pt)//形参-空指针
 {
-  LEDFLASH *ptLedFlash = (LEDFLASH *)pt;
-  byte pin = ptLedFlash -> ledpin;
-  int delayTime = ptLedFlash -> delayTime;
-    pinMode(pin, OUTPUT);
     while(1)
     {
-      digitalWrite(pin,!digitalRead(pin));
-      vTaskDelay(delayTime/portTICK_PERIOD_MS);//portTICK_PERIOD_MS将ms单位转换成时钟节拍数
+      printf("Inventory : %d\n", inventory);
+      printf("retailCount : %d\n", retailCount);//打印当前库存和销量
+
+      if(inventory == 0)
+      {
+        printf("\n----SALES SUMMARY----\n");
+        printf("Total Sales: %d\n\n", retailCount);
+      }
     }
+    vTaskDelay(pdMS_TO_TICKS(100));
 }
 
 
-
-//很明显，这样不能是的led1亮1s,led2亮2s。因为代码是一行行运行的，led1亮了4秒
 void setup() {
-  LEDFLASH led1,led2;//声明结构体
-  led1.ledpin = LED1PIN;//初始化结构体
-  led2.ledpin = LED2PIN;
-  led1.delayTime = 1000;
-  led2.delayTime = 2000;
+  Serial.begin(115200);
+  // xTaskCreate(task1, 
+  //                "Blik_10",
+  //                1024*4,
+  //                NULL,
+  //                1,
+  //                NULL);
+
+  // xTaskCreate(task2, 
+  //               "Blik_11",
+  //               1024*4,
+  //               NULL,//此一步操作就行
+  //               1,
+  //               NULL) ;
 
   if(xTaskCreate(task1, 
                  "Blik_10",
-                 1024,
-                 (void *)&led1,
+                 1024*4,//问题在这将1024改成1024*4即可，问题是如何确定内存
+                 NULL,
                  1,
                  NULL) == pdPASS)//返回值-pdPASS任务创建成功
     Serial.println("Task1 Created");
 
   if(xTaskCreate(task2, 
                 "Blik_11",
-                1024,
-                (void  *)&led2,//此一步操作就行
+                1024*4,
+                NULL,//此一步操作就行
                 1,
                 NULL) == pdPASS)//返回值-pdPASS任务创建成功
-  Serial.println("Task2 Created");
+    Serial.println("Task2 Created");
 }
 void loop() {
   // put your main code here, to run repeatedly:
