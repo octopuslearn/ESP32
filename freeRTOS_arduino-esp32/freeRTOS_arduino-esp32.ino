9e365515a4f562ac5ce9e1e6ddcf76b158a04ae8
octopuslearn <3148262242@qq.com>
2023-08-24 18:35:08 +0800

    以绝对频率运行
    实质上是获得，从启动到现在的时间计数器tick，感觉类似于millis();
    
    TickType_t xLastWakeTime = xTaskGetTickCount();
    
    xTaskDelayUntil(&xLastWakeTime , 3000);//此函数会自动更新xLastWakeTime而不用在for循环中使用

--- freeRTOS_arduino-esp32/freeRTOS_arduino-esp32.ino
+++ freeRTOS_arduino-esp32/freeRTOS_arduino-esp32.ino
@@ -1,38 +1,39 @@
+void task1(void *pt);
 
-
-TickType_t timeOut = 1000;//用于获取信号量的Timeout 1000 ticks //TickType_t是一种数据类型
-
-
-SemaphoreHandle_t xMutexInventory;//创建信号量句柄
-
-
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  //指派任务给指定核心，最后一个参数为0-核心1,1-核心2
-  if(xTaskCreatePinnedToCore(task1, 
-              "Blik_10",
-              1024*4,//问题在这将1024改成1024*4即可，问题是如何确定内存
-              NULL,
-              1,
-              NULL,
-              0) == pdPASS);//返回值-pdPASS任务创建成功
+  xTaskCreate(task1, "show", 1024, NULL, 1, NULL);
 
 }
 
-void loop() {
-
-  Serial.println(xPortGetCoreID());//--->由此可验证setpu和loop运行在cup1上//获取当前任务运行的核心
-  // put your main code here, to run repeatedly:
-}
-
-
-
-void task1(void *pt)//形参-空指针
+void loop()
 {
 
-  while(1)
+}
+
+void task1(void *pt)
+{
+  static float stockPrice = 99.57;//股票价格
+  const TickType_t xFrequency = 3000;//间隔 3000ticks = 3seconds//需要多长时间执行一次
+
+  TickType_t xLastWakeTime = xTaskGetTickCount();//TickType_t是一种重新命名的数据类型-uint32_t
+  for(;;)
   {
-    Serial.println(xPortGetCoreID());
+    Serial.println("xTackGetTickCount: ");Serial.println(xTaskGetTickCount());//用于验证是否真的运行程序是分毫不差的3s
+    /*以下，但是有个问题，执行这些程序也是需要时间的，而我们需要分毫不差*/
+    // a = xTaskGetTickCount();//获得tick，相当于millis()
+    // //1tick=1ms，要求此任务3000ms完成，那么vTickDelay(3000-runtime)
+    // b = xTaskGetTickCount();
+    // vTaskDelay(3000 - (b - a));
+    /*以上，但是有个问题，执行这些程序也是需要时间的，而我们需要分毫不差*/
+
+     // ------- 很复杂的交易股票计算，时间不定 ---------
+    stockPrice = stockPrice * (1 + random(1, 20) / 100.0); vTaskDelay(random(500, 2000));
+    Serial.print("stockPrice: "); Serial.println(stockPrice);
+
+
+    xTaskDelayUntil(&xLastWakeTime, xFrequency);//本来xLastWakeTime应该在for循环中不断更新，但是FreeRTOS可以自动更新
   }
+
 }
