5424cb6edcb64e8b68cc06850915ef08679aaf9b
octopuslearn <120025758+octopuslearn@users.noreply.github.com>
2023-08-30 19:15:32 +0800

    task中为空的话,core0默认开启看门狗，core0 core1中会有IDLE用于喂狗
    task中为空的话，会占用资源，除非Block,Suspend.
    
    core 0默认开启看门狗，5s,。IDLE任务默认开启，优先级0。当task中为空，是会占用资源，永远无法运行IDLE,即无法喂狗，5s后会自动重启。
    
    解决方法：Block或者Suspend
    task内
        //vTaskDelay(1);//法1，Block
        // TaskHandle_t taskHandle;//法2 Suspend
        // vTaskSuspend(taskHandle);

--- freeRTOS_arduino-esp32/freeRTOS_arduino-esp32.ino
+++ freeRTOS_arduino-esp32/freeRTOS_arduino-esp32.ino
@@ -1,154 +1,52 @@
+TaskHandle_t taskHandle;
 
-#include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 20, 4);
-TaskHandle_t taskHandle = NULL;//创建句柄
-int taskMem = 1024;
-void taskA(void *ptParam);
-void taskB(void *pt);
-void lcdClear();//清除屏幕
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  int heapSize = ESP.getHeapSize();//获取堆大小
-  Serial.print("heapSize: ");Serial.println(heapSize);
-  int heapFree = ESP.getFreeHeap();//获取空闲堆
-  Serial.print("heapFree: ");Serial.println(heapFree);
-
-
-  xTaskCreate(taskA,//创建完任务后，就给个这个任务一桶水，水就是内存
-            "get now freeHeap",
-            taskMem*8,
-            NULL,
-            1,
-            NULL);
-
-
-  int heapSizes = ESP.getHeapSize();//获取堆大小
-  Serial.print("任务创建后heapSize: ");Serial.println(heapSizes);
-  int heapFrees = ESP.getFreeHeap();//获取空闲堆
-  Serial.print("任务创建后heapFree: ");Serial.println(heapSizes);
-}
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void demo1();
+void taskA(void *ptParma);
 
 
 
-
-void taskA(void *ptParam)
+void setup()
 {
-  pinMode(32, INPUT_PULLUP);
-  pinMode(33, INPUT_PULLUP);
-  pinMode(25, INPUT_PULLUP);
-  pinMode(26, INPUT_PULLUP);
+  Serial.begin(9600);
+  demo1();
+}
+
+void loop()
+{
+  Serial.println("lookBack Task - Priority 1");
+}
+
+void demo1()
+{
+  //core0上运行两个任务，一个是IDLE 优先级0，一个是taskA优先级1
+  xTaskCreatePinnedToCore(taskA, "taskA", 1024*4, NULL, 1, NULL, 0);
+}
+
+void taskA(void *ptParma)
+{
+  //core0上运行两个任务，一个是IDLE 优先级0，一个是taskA 优先级1
+  //由于taskA优先级1高，taskA没有Block和Suspend所以不会释放资源，所以不会运行IDLE
+  //会重启的理由：core0默认开启看门狗，默认时间5s，由于无法运行IDLE，所以无法喂狗，所以5s重启
+  // lookBack Task - Priority 1
+// lookBack Task - Priority 1
+// lookBack Task - Priority 1
+// lookBE (10098) task_wdt: Task watchdog got triggered. The following tasks did not reset the watchdog in time:
+// E (10098) task_wdt:  - IDLE (CPU 0)
+// E (10098) task_wdt: Tasks currently running:
+// E (10098) task_wdt: CPU 0: taskA
+
+  //解决方法1：taskA Block(vTaskDelay)或者Suspend(vTaskSuspend)
+// lookBack Task - Priority 1
+// lookBack Task - Priority 1
+// lookBack Task - Priority 1
+// lookBack Task - Priority 1
+// lookBack Task - Priority 1
   while(1)
   {
-    //创建任务
-    if (digitalRead(32) == LOW) 
-    {
-      if(taskHandle == NULL)//倘若任务未创建
-      {
-        if(xTaskCreate(taskB, "LCD_show", taskMem*8,  NULL, 1, &taskHandle) == pdPASS)
-        {
-          Serial.print(xTaskGetTickCount());//获取开机至当前tick
-          Serial.println("- LOG: Task is Created.");
-        }
-        else
-        {
-          Serial.print(xTaskGetTickCount());
-          Serial.println("- WARNING: Unable to Create Task.");
-        }
-      }
-      else
-      {
-        Serial.print(xTaskGetTickCount());
-        Serial.println("- WARNING: Task **WAS** Created.");
-      }
-      vTaskDelay(120);
-    }
-    //删除任务
-    //删除任务首要条件-任务存在，任务存在依据-taskHandle的值不为空NLL
-    //1.删除任务，清空屏幕，将taskHandle句柄手动置空
-    //2.打印提示信息
-    if(digitalRead(33) == LOW)
-    {
-      if(taskHandle != NULL)//保证任务是存在的
-      {
-        vTaskDelete(taskHandle);//删除任务
-        lcdClear();//清除屏幕
-        taskHandle = NULL;//手动将taskHandle设置为空
-      }
-      if(taskHandle != NULL)//检测是否删完
-      {
-        Serial.print(xTaskGetTickCount());
-        Serial.println("- WARNING: Unable to Delete Task.");
-      }
-      else
-      {
-        Serial.print(xTaskGetTickCount());
-        Serial.println("- LOG: Task is Deleted.");
-      }
-      vTaskDelay(120);
-    }
-    //暂停任务
-    //1.首要条件-任务存在，任务存在依据-taskHandle的值不为空NLL
-    //2.暂停任务 vTaskSuspend
-    if(digitalRead(25) == LOW)
-    {
-      if(taskHandle != NULL)
-      {
-        vTaskSuspend(taskHandle);//暂停任务
-        Serial.print(xTaskGetTickCount());
-        Serial.println("- LOG: Task is suspended.");
-      }
-      else
-      {
-        Serial.print(xTaskGetTickCount());
-         Serial.println("- WARNING: Unable to suspended Task.");
-      }
-      vTaskDelay(120);
-    }
-    //恢复任务
-    if(digitalRead(26) == LOW)
-    {
-      if(taskHandle != NULL)
-      {
-        vTaskResume(taskHandle);//恢复任务
-        Serial.print(xTaskGetTickCount());
-        Serial.println("- LOG: Task is resumed.");
-      }
-      else
-      {
-        Serial.print(xTaskGetTickCount());
-        Serial.println("- WARNING: Unable to resumed Task.");
-      }
-      vTaskDelay(120);
-    }
+    //vTaskDelay(1);//法1，Block
+    // TaskHandle_t taskHandle;//法2 Suspend
+    // vTaskSuspend(taskHandle);
   }
 }
 
 
-void taskB(void *pt)
-{
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(0,0);//设定光标位置
-  lcd.print(" Bilibili   Channel ");
-  lcd.setCursor(0, 1);
-  lcd.print("-FreeRTOS on EPS32- ");
-  lcd.setCursor(0, 2);
-  lcd.print("Study Hard  &  Smart");
-  while(1)
-  {
-    lcd.setCursor(9, 3);
-    lcd.print(xTaskGetTickCount()/100);//屏幕加载值更新速度
-    vTaskDelay(100);
-  }
-}
-  void lcdClear()
-  {
-    lcd.clear();
-    lcd.noBacklight();
-    lcd.setCursor(0, 0);
-    lcd.print(" ");
-  }
