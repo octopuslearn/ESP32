57d9dfb5f85c1a6c3b4594c63c4ce858c828f97d
octopuslearn <120025758+octopuslearn@users.noreply.github.com>
2023-09-01 18:46:15 +0800

    删除任务之理由   Event Group事件组 创建句柄，创建任务组，设定事件组相应位，设定完后判断相应的位，等待相应的位，执行完任务后将此任务位置1，删除任务
    /********以下，删除任务之理由*******/
    //添加进购物车
    // void consumerA(void *ptParma)
    // {
    //   EventBits_t uxBits;//用于存放事件组 24bits 的值 EventBits_t-uin16_t或者uin32_t
    
    //   while(1)
    //   {
    //     uxBits = xEventGroupSetBits(xEventPurchase, ADDTOCART_0);//将ADDTOCART_0设置为1
    //     if(uxBits & ADDTOCART_0)//判断是否设置完成？？？
    //     {
    //       Serial.println("商品已经添加到了购物车，付款中...");
    //       Serial.print("   Event Group Value:");
    //       Serial.println(uxBits ,BIN);//以二进制格式打印
    //     }
    //     vTaskDelete(NULL);//删除任务理由：不删除的话，会一直执行这个任务
    //   }
    // }
    /********以上，删除任务之理由*******/
    
    EventGroupHandle_t xEventPurchase = NULL;//创建事件组
    
    EventPurchase = xEventGroupCreate();//创建 event group-事件组
    
    EventBits_t uxBits;//用于存放事件组 24bits 的值 EventBits_t-uin16_t或者uin32_t
    
        uxBits = xEventGroupSetBits(xEventPurchase, ADDTOCART_0);//将ADDTOCART_0设置为1
        if(uxBits & ADDTOCART_0)//判断是否设置完成？？？
        {
    
    vTaskDelete(NULL);//删除任务理由：不删除的话，会一直执行这个任务
    、
    
        //等待事件组中相应的位置1，即等待添加到购物车完成
        uxBits = xEventGroupWaitBits(xEventPurchase,//Event Group Hanler-事件组句柄
                                    ADDTOCART_0,    //等待事件组中的那个bit置1，即ADDTOCART_0 = 1
                                    pdFALSE,        //执行后，对应的的bits是否重置为0
                                    pdTRUE,         //等待的bit的关系，True-&，Fall-|
                                    TimeOut);
    
    uxBits = xEventGroupSetBits(xEventPurchase, INVENTORY_2);//将INVENTORY_2置1

--- freeRTOS_arduino-esp32/freeRTOS_arduino-esp32.ino
+++ freeRTOS_arduino-esp32/freeRTOS_arduino-esp32.ino
@@ -1,56 +1,151 @@
-SemaphoreHandle_t xSemaLED = NULL;//创建信号量句柄
+/********以下，删除任务之理由*******/
+//添加进购物车
+// void consumerA(void *ptParma)
+// {
+//   EventBits_t uxBits;//用于存放事件组 24bits 的值 EventBits_t-uin16_t或者uin32_t
+  
+//   while(1)
+//   {
+//     uxBits = xEventGroupSetBits(xEventPurchase, ADDTOCART_0);//将ADDTOCART_0设置为1
+//     if(uxBits & ADDTOCART_0)//判断是否设置完成？？？
+//     {
+//       Serial.println("商品已经添加到了购物车，付款中...");
+//       Serial.print("   Event Group Value:");
+//       Serial.println(uxBits ,BIN);//以二进制格式打印
+//     }
+//     vTaskDelete(NULL);//删除任务理由：不删除的话，会一直执行这个任务
+//   }
+// }
+/********以上，删除任务之理由*******/
 
-void consumer(void *ptParma);
-void production(void *ptParma);
-
-String consumerA = "JD";
-String consumerB = "TMALL";
-String consumerC = "PDD";
 
 
-void consumer(void *ptParma)
+#define ADDTOCART_0 (1<<0)
+#define PAYMENT_1   (1<<1)
+#define INVENTORY_2 (1<<2)
+
+#define ALLBITS 0xffffff  //24bits都是1
+
+EventGroupHandle_t xEventPurchase = NULL;//创建事件组
+const TickType_t TimeOut = 1000;
+
+
+void button(void *ptParma);
+void consumerA(void *ptParma);//添加进购物车
+void consumerB(void *ptParma);//支付
+void consumerC(void *ptParma);//出货
+
+
+void button(void *ptParma)
 {
-  String website = *(String *)ptParma;//恢复参数
+  pinMode(22, INPUT_PULLUP);
   while(1)
   {
-    if(xSemaphoreTake(xSemaLED, portMAX_DELAY) == pdTRUE)//消费者-1返回pdPASS,或者pdFAIL
+    if(digitalRead(22) == LOW)//消费者-1返回pdPASS,或者pdFAIL
     {
-      for (int i = 0; i < random(200, 400); i++) vTaskDelay(10);
-      Serial.print(website);
-      Serial.println("抢到并销售一台: ");
+      
+      Serial.println("用户真心决定下单了...");
+
+      //放一些随机的延迟，否则运行的太快了，看不出效果      
+      for (int i = 0; i < random(100, 200); i++) vTaskDelay(10);
+      //添加进购物车
+      xTaskCreate(consumerA,  "consumer a",   1024*6, NULL, 1, NULL);
+      //支付
+      xTaskCreate(consumerB,  "consumer b",   1024*6, NULL, 1, NULL);
+      //出货
+      xTaskCreate(consumerC,  "consumer c",   1024*6, NULL, 1, NULL);
+    
+    vTaskDelay(100);
     }
   }
 }
 
-void production(void *ptParma)
+
+//添加进购物车
+void consumerA(void *ptParma)
 {
+  EventBits_t uxBits;//用于存放事件组 24bits 的值 EventBits_t-uin16_t或者uin32_t
+  
   while(1)
   {
-     for (int i = 0; i < random(100, 200); i++) vTaskDelay(10);
-    xSemaphoreGive(xSemaLED);
-    Serial.println("...... 手机再放出一台,");
+    uxBits = xEventGroupSetBits(xEventPurchase, ADDTOCART_0);//将ADDTOCART_0设置为1
+    if(uxBits & ADDTOCART_0)//判断是否设置完成？？？
+    {
+      Serial.println("商品已经添加到了购物车，付款中...");
+      Serial.print("   Event Group Value:");
+      Serial.println(uxBits ,BIN);//以二进制格式打印
+    }
+    vTaskDelete(NULL);//删除任务理由：不删除的话，会一直执行这个任务
+  }
+}
+
+//支付
+void consumerB(void *ptParma)//完成添加到购物车后才能支付
+{
+  EventBits_t uxBits;
+  while(1)
+  {
+    //###问：问什么等待完成后，还要再判断以一次相应的位是否置1，岂不是多此一举？？？
+    //等待事件组中相应的位置1，即等待添加到购物车完成
+    uxBits = xEventGroupWaitBits(xEventPurchase,//Event Group Hanler-事件组句柄
+                                ADDTOCART_0,    //等待事件组中的那个bit置1，即ADDTOCART_0 = 1
+                                pdFALSE,        //执行后，对应的的bits是否重置为0
+                                pdTRUE,         //等待的bit的关系，True-&，Fall-|
+                                TimeOut);
+
+    if(uxBits & ADDTOCART_0)
+    {
+      //随机延迟, 模拟付款验证过程
+      for (int i = 0; i < random(200, 400); i++) vTaskDelay(10);
+      uxBits = xEventGroupSetBits(xEventPurchase, PAYMENT_1);//当前任务完成后在事件组中将相应的位置1，即将bit1 PAYMENT_1设置为1
+      
+      Serial.println("支付宝付款完成,可以出货...");
+      Serial.print("   Event Group Value:");
+      Serial.println(uxBits, BIN);
+
+      //###问：为什么要吧任务删除呢,之前的代码中几乎没有过用完后就删除
+      vTaskDelete(NULL);//删除此任务//完成当前任务后将任务删除
+    }
+  }
+}
+
+//出货
+void consumerC(void *ptParma)
+{
+  
+  EventBits_t uxBits;
+
+  while(1)
+  {
+    uxBits = xEventGroupWaitBits(xEventPurchase,
+                                ADDTOCART_0|PAYMENT_1,
+                                pdFALSE,
+                                pdTRUE,
+                                TimeOut);
+    if((uxBits & ADDTOCART_0) && (uxBits & PAYMENT_1))//***注意这里
+    //错误 if(uxBits & (ADDTOCART_0|PAYMENT_1))//消费者-1返回pdPASS,或者pdFAIL
+    {
+      for (int i = 0; i < random(100, 200); i++) vTaskDelay(10);
+      uxBits = xEventGroupSetBits(xEventPurchase, INVENTORY_2);//将INVENTORY_2置1
+      
+      Serial.println("仓库出货完成,快递已取货...");
+      Serial.print("   Event Group Value:");
+      Serial.println(uxBits, BIN);
+
+      vTaskDelete(NULL);
+    }
   }
 }
 
 
+
+
 void setup() {
   Serial.begin(9600);
+  
+  xEventPurchase = xEventGroupCreate();//创建 event group-事件组
 
-  xSemaLED = xSemaphoreCreateCounting(3, 0);//创建计数累计信号量,3-MAX,0-当前库存
-
-  if(xSemaLED == NULL)//信号量创建失败
-  {
-        printf("No Enough Ram, Unable to Create Semaphore.");
-  } 
-  else 
-  {
-    xTaskCreate(production,"production",   1024*6, NULL,               1, NULL);
-    xTaskCreate(consumer,  "consumer a",   1024*6, (void *)&consumerA, 1, NULL);
-    xTaskCreate(consumer,  "consumer b",   1024*6, (void *)&consumerB, 1, NULL);
-    xTaskCreate(consumer,  "consumer c",   1024*6, (void *)&consumerC, 1, NULL);
-    // xTaskCreate(production,"production",   1024*6, NULL,               1, NULL);
-  }
-
+  xTaskCreate(button,"button",   1024*6, NULL,               1, NULL);
 
 }
 void loop() {
