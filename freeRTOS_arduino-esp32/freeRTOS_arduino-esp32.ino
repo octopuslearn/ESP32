c3d63e4897d08b04ab30d4b42fe0ac1e5f01b348
octopuslearn <120025758+octopuslearn@users.noreply.github.com>
2023-09-01 14:17:32 +0800

    Binary semaphore 二进制信号量 创建句柄，创建二进制信号量，发送（生产者+1），接收（消费者-1)
    二进制信号量是最简单的一种
    
    SemaphoreHandle_t  xSemaLED = NULL;//创建信息量句柄
    
    TickType_t timeOut = 100;//设定时间
    
    xSemaLED  = xSemaphoreCreateBinary();//创建二进制信号量//返回NULL,或者handle
    
    xSemaphoreGive(xSemaLED）//发送信号量//生产者+1
    
    if(xSemaphoreTake(xSemaLED  ,timeOut) == pdTRUE)//用于获取信号量//消费者-1，返回pdPASS,或者pdFAIL

--- freeRTOS_arduino-esp32/freeRTOS_arduino-esp32.ino
+++ freeRTOS_arduino-esp32/freeRTOS_arduino-esp32.ino
@@ -1,145 +1,52 @@
-#include "DHTesp.h"
-#include <LiquidCrystal_I2C.h>
-#define DHT22_ID 0
-#define LDR_ID 1
+SemaphoreHandle_t xSemaLED = NULL;//创建信号量句柄
 
-LiquidCrystal_I2C lcd(0x27, 20, 4);
-int taskMem = 1024;
+TickType_t timeOut = 1000;
 
-
-//定义结构体
-typedef struct{
-  byte deviceID;
-  float value1;
-  float value2;
-}SENSOR;
-
-
-//此处参数传递错误 QueueHandle_t queueMsq = xQueueCreate(8, sizeof(char[20]));//创建队列，长度8，尺寸sizeof(char[20])////对于lcd一行能够显示20个字符，存了8行。但是实际上每行20个字符，4行是极限
-QueueHandle_t queueMsq = xQueueCreate(8, sizeof(SENSOR));//创建队列，长度8，尺寸sizeof(char[20])////对于lcd一行能够显示20个字符，存了8行。但是实际上每行20个字符，4行是极限
-
-
-
-void dht22(void *ptParam)
+void button(void *ptParma);
+void led(void *ptParma);
+void button(void *ptParma)
 {
-  const byte dhtPin = 32;
-  DHTesp dhtSensor;
-  dhtSensor.setup(dhtPin, DHTesp::DHT22);
-
-
-  SENSOR dht22Sensor;//声明结构体
-  dht22Sensor.deviceID =  DHT22_ID;//初始化结构体
-
+  pinMode(22, INPUT_PULLUP);
   while(1)
   {
-    TempAndHumidity  data = dhtSensor.getTempAndHumidity();
-
-    dht22Sensor.value1 = data.temperature;
-    dht22Sensor.value2 = data.humidity;
-
-    TickType_t timeOut = 2000;
-    //错误 if(xQueueSend(queueMsq, &htSensor.deviceID, timeOut) == pdPASS)
-    if(xQueueSend(queueMsq, &dht22Sensor, timeOut) != pdPASS)
+    if(digitalRead(22) == LOW)
     {
-      Serial.print("dht22: xQueue is full.");
+      xSemaphoreGive(xSemaLED);//发送信号
+      vTaskDelay(100);
     }
-
-    vTaskDelay(1000);
   }
 }
 
-
-
-void ldr(void *ptParam) 
+void led(void *ptParma)
 {
-  const float GAMMA = 0.7;
-  const float RL10 = 50;
-  const byte ldrPIN = 27;
-  pinMode(ldrPIN, INPUT);
-
-
-  SENSOR ldrSensor;//声明结构体
-  ldrSensor.deviceID = LDR_ID;
-
+  pinMode(23, OUTPUT);
   while(1)
   {
-    int analogValue = analogRead(ldrPIN);
-
-    float voltage = analogValue / 4095. * 5;
-    float resistance = 2000 * voltage / (1 - voltage / 5);
-    float lux = pow(RL10 * 1e3 * pow(10, GAMMA) / resistance, (1 / GAMMA));
-
-    ldrSensor.value1 = lux;
-    ldrSensor.value2 = 0.0;
-
-    TickType_t timeOut = 2000;
-    if(xQueueSend(queueMsq, &ldrSensor, timeOut) != pdPASS)
+    if(xSemaphoreTake(xSemaLED, timeOut) == pdTRUE)//消费者-1返回pdPASS,或者pdFAIL
     {
-      Serial.print("ldr: xQueue is full.");
+      digitalWrite(23,!digitalRead(23));
+      vTaskDelay(1000);
     }
-
-    vTaskDelay(1000);
   }
 }
-
-//LCD主体任务，因为LCD是接收，所以xQueueReceive
-void lcdTask(void *ptParam)
-{
-  lcd.init();
-  lcd.backlight();
-
-  lcd.setCursor(0, 0);
-  lcd.print("   LONELY  BINARY  ");
-
-  SENSOR data;//声明结构体
-
-  while(1)
-  {
-    TickType_t timeOut = 2000;
-    //此处写错了if(xQueueReceive(queueMsq, &data, timeOut) != pdPASS)
-    if(xQueueReceive(queueMsq, &data, timeOut) == pdPASS)//当timeOut内不为空时才，显示
-    {
-      switch(data.deviceID)
-      //错的switch(data)
-      {
-        case DHT22_ID:
-          lcd.setCursor(0, 1);
-          lcd.print("Temp: " + String(data.value1, 2) + "c");
-          lcd.setCursor(0, 2);
-          lcd.print("Humidity: " + String(data.value2, 1) + "%");
-          break;
-        case LDR_ID:
-          lcd.setCursor(0, 3);
-          if (data.value1 > 50) {
-            lcd.print("Bright ");
-          } else {
-            lcd.print("Dark ");
-          }
-          //lcd.setCursor(0, 3);
-          lcd.print(String(data.value1, 2) + " lux");
-          break;
-        default:
-          Serial.println("LCD: Unkown Device");
-          break;
-      }
-    }
-    else {
-      Serial.println("LCD: Message Queue is Empty");
-    };
-
-    vTaskDelay(2000);
-  }
-}
-
 
 
 
 void setup() {
   Serial.begin(9600);
 
-  xTaskCreate(dht22,  "dht22",  taskMem*4,NULL,1,NULL);
-  xTaskCreate(ldr,    "ldr",    taskMem*4,NULL,1,NULL);
-  xTaskCreate(lcdTask,"lcdTask",taskMem*8,NULL,1,NULL);
+  xSemaLED = xSemaphoreCreateBinary();//创建二进制信号量,返回NULL或者handler
+
+  if(xSemaLED == NULL)//信号量创建失败
+  {
+        printf("No Enough Ram, Unable to Create Semaphore.");
+  } 
+  else 
+  {
+    xTaskCreate(led, "led", 1024*4, NULL, 1, NULL);
+    xTaskCreate(button, "button", 1024*4, NULL, 1, NULL);
+  }
+
 }
 void loop() {
   // put your main code here, to run repeatedly:
