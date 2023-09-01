60c6042a1acfa0cfd7997176f10f4689733cf214
octopuslearn <120025758+octopuslearn@users.noreply.github.com>
2023-09-01 14:39:32 +0800

    续，中断方式（更加节省了资源） 两点不同：获取按下时的时间，防止抖动使用。其二，中断触发给信号
    void setup()
    {    pinMode(22, INPUT_PULLUP);
        attachInterrupt(22, ISR, FALLING);
    }
    
    void IRAM_ATTR ISR()
    {
      btnDeounce = xTaskGetTickCountFromISR();//获取当前按下时的时间
      xSemaphoreGiveFromISR(xSemaLED, NULL);//给一个信号量，即变成1
    }
    
        if(xSemaphoreTake(xSemaLED, timeOut) == pdTRUE)//消费者-1返回pdPASS,或者pdFAIL
        {
          if(xTaskGetTickCount() - btnDeounce <100)//防止多次按下，防抖动用的
          {
            digitalWrite(23, !digitalRead(23));
            vTaskDelay(1000);
          }

--- freeRTOS_arduino-esp32/freeRTOS_arduino-esp32.ino
+++ freeRTOS_arduino-esp32/freeRTOS_arduino-esp32.ino
@@ -1,33 +1,45 @@
 SemaphoreHandle_t xSemaLED = NULL;//创建信号量句柄
 
 TickType_t timeOut = 1000;
+volatile TickType_t btnDeounce = 0;//用于 button Debounce
+
 
 void button(void *ptParma);
 void led(void *ptParma);
-void button(void *ptParma)
-{
-  pinMode(22, INPUT_PULLUP);
-  while(1)
-  {
-    if(digitalRead(22) == LOW)//###发现按键按下去了，则给了个信号量--->内存中的这个量就变成1
-    {
-      xSemaphoreGive(xSemaLED);
-      vTaskDelay(100);
-    }
-  }
-}
+// void button(void *ptParma)
+// {
+//   pinMode(22, INPUT_PULLUP);
+//   while(1)
+//   {
+//     if(digitalRead(22) == LOW)
+//     {
+//       xSemaphoreGive(xSemaLED);//发送信号
+//       vTaskDelay(100);
+//     }
+//   }
+// }
 
 void led(void *ptParma)
 {
   pinMode(23, OUTPUT);
   while(1)
   {
-    if(xSemaphoreTake(xSemaLED, timeOut) == pdTRUE)//###判断当前信号量，是1则执行下面的程序，并且将1变成了0
+    if(xSemaphoreTake(xSemaLED, timeOut) == pdTRUE)//消费者-1返回pdPASS,或者pdFAIL
     {
-      digitalWrite(23,!digitalRead(23));
-      vTaskDelay(1000);
+      if(xTaskGetTickCount() - btnDeounce <100)//防止多次按下，防抖动用的
+      {
+        digitalWrite(23, !digitalRead(23));
+        vTaskDelay(1000);
+      }
     }
   }
+}
+
+
+void IRAM_ATTR ISR()
+{
+  btnDeounce = xTaskGetTickCountFromISR();//获取当前按下时的时间
+  xSemaphoreGiveFromISR(xSemaLED, NULL);//给一个信号量，即变成1
 }
 
 
@@ -44,9 +56,11 @@ void setup() {
   else 
   {
     xTaskCreate(led, "led", 1024*4, NULL, 1, NULL);
-    xTaskCreate(button, "button", 1024*4, NULL, 1, NULL);
+    // xTaskCreate(button, "button", 1024*4, NULL, 1, NULL);
   }
 
+    pinMode(22, INPUT_PULLUP);
+    attachInterrupt(22, ISR, FALLING);
 }
 void loop() {
   // put your main code here, to run repeatedly:
