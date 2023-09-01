8f5fe8d5cefee2a405546d9c4e82852f9f7f61a9
octopuslearn <120025758+octopuslearn@users.noreply.github.com>
2023-09-01 15:46:54 +0800

    信息量节省内存理由。。。Counting Semaphore计数累计信息量    实际上信息量是int
    注意：
        if(xSemaphoreTake(xSemaLED, 1000) == pdTRUE)//消费者-1返回pdPASS,或者pdFAIL//也可以
        // if(xSemaphoreTake(xSemaLED, portMAX_DELAY) == pdTRUE)//消费者-1返回pdPASS,或者pdFAIL
    
    if(xSemaphoreTake(xSemaLED, portMAX_DELAY) == pdTRUE)
    使用此种方法节省内存
    
    xSemaphoreCreateCounting();//创建累计计数信息量，3-MAX,0-当前库存
    
    实际上信息量就是一个Int
    而累计计数信息量就是一个int
    give +1
    take -1
    
    直到等于0就不继续执行了

--- freeRTOS_arduino-esp32/freeRTOS_arduino-esp32.ino
+++ freeRTOS_arduino-esp32/freeRTOS_arduino-esp32.ino
@@ -1,53 +1,42 @@
 SemaphoreHandle_t xSemaLED = NULL;//创建信号量句柄
 
-TickType_t timeOut = 1000;
-volatile TickType_t btnDeounce = 0;//用于 button Debounce
+void consumer(void *ptParma);
+void production(void *ptParma);
+
+String consumerA = "JD";
+String consumerB = "TMALL";
+String consumerC = "PDD";
 
 
-void button(void *ptParma);
-void led(void *ptParma);
-// void button(void *ptParma)
-// {
-//   pinMode(22, INPUT_PULLUP);
-//   while(1)
-//   {
-//     if(digitalRead(22) == LOW)
-//     {
-//       xSemaphoreGive(xSemaLED);//发送信号
-//       vTaskDelay(100);
-//     }
-//   }
-// }
-
-void led(void *ptParma)
+void consumer(void *ptParma)
 {
-  pinMode(23, OUTPUT);
+  String website = *(String *)ptParma;//恢复参数
   while(1)
   {
-    if(xSemaphoreTake(xSemaLED, timeOut) == pdTRUE)//消费者-1返回pdPASS,或者pdFAIL
+    if(xSemaphoreTake(xSemaLED, portMAX_DELAY) == pdTRUE)//消费者-1返回pdPASS,或者pdFAIL
     {
-      if(xTaskGetTickCount() - btnDeounce <100)//防止多次按下，防抖动用的
-      {
-        digitalWrite(23, !digitalRead(23));
-        vTaskDelay(1000);
-      }
+      for (int i = 0; i < random(200, 400); i++) vTaskDelay(10);
+      Serial.print(website);
+      Serial.println("抢到并销售一台: ");
     }
   }
 }
 
-
-void IRAM_ATTR ISR()
+void production(void *ptParma)
 {
-  btnDeounce = xTaskGetTickCountFromISR();//获取当前按下时的时间
-  xSemaphoreGiveFromISR(xSemaLED, NULL);//给一个信号量，即变成1
+  while(1)
+  {
+     for (int i = 0; i < random(100, 200); i++) vTaskDelay(10);
+    xSemaphoreGive(xSemaLED);
+    Serial.println("...... 手机再放出一台,");
+  }
 }
-
 
 
 void setup() {
   Serial.begin(9600);
 
-  xSemaLED = xSemaphoreCreateBinary();//创建二进制信号量,返回NULL或者handler
+  xSemaLED = xSemaphoreCreateCounting(3, 0);//创建计数累计信号量,3-MAX,0-当前库存
 
   if(xSemaLED == NULL)//信号量创建失败
   {
@@ -55,12 +44,14 @@ void setup() {
   } 
   else 
   {
-    xTaskCreate(led, "led", 1024*4, NULL, 1, NULL);
-    // xTaskCreate(button, "button", 1024*4, NULL, 1, NULL);
+    xTaskCreate(production,"production",   1024*6, NULL,               1, NULL);
+    xTaskCreate(consumer,  "consumer a",   1024*6, (void *)&consumerA, 1, NULL);
+    xTaskCreate(consumer,  "consumer b",   1024*6, (void *)&consumerB, 1, NULL);
+    xTaskCreate(consumer,  "consumer c",   1024*6, (void *)&consumerC, 1, NULL);
+    // xTaskCreate(production,"production",   1024*6, NULL,               1, NULL);
   }
 
-    pinMode(22, INPUT_PULLUP);
-    attachInterrupt(22, ISR, FALLING);
+
 }
 void loop() {
   // put your main code here, to run repeatedly:
