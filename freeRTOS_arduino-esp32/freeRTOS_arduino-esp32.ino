d8738aa3068ab8e616f7fc4f6ad68c82d14217b5
octopuslearn <120025758+octopuslearn@users.noreply.github.com>
2023-09-03 10:05:07 +0800

    直接任务通知作邮箱 很简单
    //所谓邮箱-将数据打包（打包方法：将数据移位进直接消息通知的uint32_t）发送xTaskNotify(xTaskNeoRing,rgb,eSetValueWithOverwrite)覆盖,接收后结包（移位）xTaskNotifyWait(0x00,0x00,&srgb,0)

--- freeRTOS_arduino-esp32/freeRTOS_arduino-esp32.ino
+++ freeRTOS_arduino-esp32/freeRTOS_arduino-esp32.ino
@@ -1,89 +1,87 @@
-//注意：创建任务时，每个参数一定要传递正确
-  //此处最后一个参数错误最后一个参数都是NULL-为啥错了就不行 xTaskCreate(taskWait, "taskWait", 1024*4, NULL, 1, &xTaskGive);
-  // xTaskCreate(taskWait, "taskWait", 1024*4, NULL, 1, &xTaskWait);
+//所谓邮箱-将数据打包（打包方法：将数据移位进直接消息通知的uint32_t）发送xTaskNotify(xTaskNeoRing,rgb,eSetValueWithOverwrite)覆盖,接收后结包（移位）xTaskNotifyWait(0x00,0x00,&srgb,0)
 
 
-//notification 只能操作value,不能操作state.因为state是系统自己操作的
-// api:xTaskNotify(xTaskWaite, 0, eIncrement);//将对方value +1
-// api:xResult = xTaskNotifyWait(0x00,//当接收到这个命令之前，notification要不要被清除清除那几位。。。0x00即不清除
-// 0x00,//运行notification后，要不要清除notification清除哪几位，0x00不清除//重置所有的bits 0x00 or ULONG_MAX 0r 0xffffffff
-// &ulNotificationValue,//将接收到的值存在这里//重置前的notification value
-// protMAX_DELAY);//block不占用cpu资源//一直等待，直到接收到nocation 为止
+#include <Adafruit_NeoPixel.h>
+#define NEOPIN 33
+#define NUMPIXELS 16
 
-void taskGive(void *ptParam);
-void taskWait(void *ptParam);
+#define RPIN 34
+#define GPIN 35
+#define BPIN 32
+#define SPIN 36 // speed
 
-static TaskHandle_t xTaskWait = NULL;
-static TaskHandle_t xTaskGive = NULL;
+static TaskHandle_t xTaskNeoRing = NULL;
+
+void configTask(void *pvParam);
+void neoRing(void *pvParam);
+
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  xTaskCreate(configTask, "Configration", 1024*10, NULL, 1, NULL);
+  xTaskCreate(neoRing, "Neo Ring", 1024*20, NULL, 1, &xTaskNeoRing);
 
-  xTaskCreate(taskGive, "taskGive", 1024*4, NULL, 1, &xTaskGive);
-  //此处最后一个参数错误最后一个参数都是NULL-为啥错了就不行 xTaskCreate(taskWait, "taskWait", 1024*4, NULL, 1, &xTaskGive);
-  xTaskCreate(taskWait, "taskWait", 1024*4, NULL, 1, &xTaskWait);
+  vTaskDelete(NULL);//删除任务
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() {  // put your main code here, to run repeatedly:
+
 }
 
 
-
-
-
-
-void taskGive(void *ptParam)
+void configTask(void *pvParam)
 {
-  pinMode(23, INPUT_PULLUP);
+  pinMode(RPIN, INPUT);
+  pinMode(GPIN, INPUT);
+  pinMode(BPIN, INPUT);
+  pinMode(SPIN, INPUT);
 
-  BaseType_t xResult;
+  uint8_t r_value = 0;
+  uint8_t g_value = 0;
+  uint8_t b_value = 0;
+  uint8_t s_value = 100;
   while(1)
   {
-    if (digitalRead(23) == LOW) 
-    {
-      // xResult = xTaskNotify(xTaskWait, 0, eIncrement);//Increment增量，即累加//对方的notification value +1
-      
-      // xResult = xTaskNotify(xTaskWait, 0, eNoAction);//不设置对方的notification value
-      //  xResult = xTaskNotify(xTaskWait, (1UL << 4UL), eSetBits);//在原有的基础上将第5位置1//16
-                                                              //成功
-      //xResult = xTaskNotify(xTaskWait, 0b110,eSetValueWithOverwrite);//覆盖-不是在原有基础上操作，是直接设置110 //6  //不管有没有被处理
+    r_value = map(analogRead(RPIN), 0, 4095, 0, 255);
+    g_value = map(analogRead(GPIN), 0, 4095, 0, 255);
+    b_value = map(analogRead(BPIN), 0, 4095, 0, 255);
+    s_value = map(analogRead(SPIN), 0, 4095, 10, 200);
 
-//demo5
-      // xResult = xTaskNotify(xTaskWait, (1UL << 4UL), eSetBits);//在原有的基础上将第5位置1//16------》》》未被处理
-      // xResult = xTaskNotify(xTaskWait, 0b010,eSetValueWithoutOverwrite);//查看在此之前的notification有没有被处理，如果有的话就不会覆盖//16
-                                                                                                                                  //失败
-      
-      
-      
-      
-      Serial.println(xResult ==  pdPASS ? "成功\n" : "失败\n");
+    uint32_t rgb = (s_value <<24) + (b_value << 16) + (g_value << 8) + (r_value << 0);//打包
 
-      vTaskDelay(120);
-    }
+    xTaskNotify(xTaskNeoRing, rgb, eSetValueWithOverwrite);//覆盖
+
+    vTaskDelay(100);
   }
-  
 }
-
-
-void taskWait(void *ptParam)
+void neoRing(void *pvParam)
 {
-  uint32_t ulNotificationValue;//用来存放任务的4个字节的notification value
-  BaseType_t xReslt;
+  Adafruit_NeoPixel pixels(NUMPIXELS, NEOPIN, NEO_GRB + NEO_KHZ800);
+  pixels.begin();
+
+  uint32_t srgb = 0;
+  uint8_t sr = 0;
+  uint8_t sg = 0;
+  uint8_t sb = 0;
+  uint8_t ss = 100;
+
   while(1)
   {
-    xReslt = xTaskNotifyWait(0x00,//在运行这个指令前，要不要清除notification,清除哪几位。。。0x00不清除
-                                   0x00,//运行notification后，要不要清除notification清除哪几位。。。0x00不清除//重置所有的bits 0x00 or ULONG_MAX or 0xffffffff
-                                   &ulNotificationValue,//将接受到的值存在这里//重置前的notification value
-                                   portMAX_DELAY);//一直等待，直到有通知来。此时Block状态，不占用cpu
-    if(xReslt == pdTRUE)
+    pixels.clear();
+    for(int i = 0;i < NUMPIXELS; i++)
     {
-      Serial.println(ulNotificationValue);
-    }
-    else 
-    {
-      Serial.println("Timeout");
+      xTaskNotifyWait(0x00, 0x00, &srgb, 0);
+      ss = (srgb & 0xff000000) >> 24;//解包
+      sg = (srgb & 0x00ff0000) >> 16;
+      sb = (srgb & 0x0000ff00) >> 8;
+      sr = (srgb & 0x000000ff) >> 0;
+
+      pixels.setPixelColor(i, pixels.Color(sr, sg, sb));
+      pixels.show();
+
+      vTaskDelay(ss*5);
     }
   }
 }
