9f4eb3f2e2f4f8d0844e14112606ad46f906cf9a
octopuslearn <120025758+octopuslearn@users.noreply.github.com>
2023-09-03 10:29:17 +0800

    直接任务通知代替二进制信号量
    由于直接任务通知没有中间量，所以更快，更清楚
    
    xTaskNoticyGive(xflashLED);//相当于xTaskNoticy()+eIncreament//也就是xSemaphoreGive();
    
    注意：ulTaskNotifyTake(pdTRUE,protMAX_DELAY)返回的是运行这行代码之前的值
    
        //注意：此函数返回的是运行这个函数之前的值，也就是说是1
        //命令含义：waitting for notification,then reset
        ulNotificationValue = ulTaskNotifyTake(pdTRUE,//运行完后，清零
                                               portMAX_DELAY);
    
        if(ulNotificationValue > 0)
        {
          digitalWrite(23, !digitalRead(23));
          vTaskDelay(1000);
        }

--- freeRTOS_arduino-esp32/freeRTOS_arduino-esp32.ino
+++ freeRTOS_arduino-esp32/freeRTOS_arduino-esp32.ino
@@ -1,87 +1,50 @@
-//所谓邮箱-将数据打包（打包方法：将数据移位进直接消息通知的uint32_t）发送xTaskNotify(xTaskNeoRing,rgb,eSetValueWithOverwrite)覆盖,接收后结包（移位）xTaskNotifyWait(0x00,0x00,&srgb,0)
-
-
-#include <Adafruit_NeoPixel.h>
-#define NEOPIN 33
-#define NUMPIXELS 16
-
-#define RPIN 34
-#define GPIN 35
-#define BPIN 32
-#define SPIN 36 // speed
-
-static TaskHandle_t xTaskNeoRing = NULL;
-
-void configTask(void *pvParam);
-void neoRing(void *pvParam);
+TaskHandle_t xflashLED = NULL;
+void flashLED(void *pvParam);
+void ReadBth(void *pvParam);
 
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  xTaskCreate(configTask, "Configration", 1024*10, NULL, 1, NULL);
-  xTaskCreate(neoRing, "Neo Ring", 1024*20, NULL, 1, &xTaskNeoRing);
-
-  vTaskDelete(NULL);//删除任务
+  xTaskCreate(flashLED,"Flash LED", 1024*4, NULL, 1, &xflashLED);
+  xTaskCreate(ReadBth,"Read Button", 1024*4, NULL, 1, NULL);
 }
 
-void loop() {  // put your main code here, to run repeatedly:
-
+void loop() {
+  // put your main code here, to run repeatedly:
 }
 
 
-void configTask(void *pvParam)
+
+void flashLED(void *pvParam)
 {
-  pinMode(RPIN, INPUT);
-  pinMode(GPIN, INPUT);
-  pinMode(BPIN, INPUT);
-  pinMode(SPIN, INPUT);
-
-  uint8_t r_value = 0;
-  uint8_t g_value = 0;
-  uint8_t b_value = 0;
-  uint8_t s_value = 100;
+  pinMode(23, OUTPUT);
+  uint32_t ulNotificationValue;
   while(1)
   {
-    r_value = map(analogRead(RPIN), 0, 4095, 0, 255);
-    g_value = map(analogRead(GPIN), 0, 4095, 0, 255);
-    b_value = map(analogRead(BPIN), 0, 4095, 0, 255);
-    s_value = map(analogRead(SPIN), 0, 4095, 10, 200);
+    //注意：此函数返回的是运行这个函数之前的值，也就是说是1
+    //命令含义：waitting for notification,then reset
+    ulNotificationValue = ulTaskNotifyTake(pdTRUE,//运行完后，清零
+                                           portMAX_DELAY);
 
-    uint32_t rgb = (s_value <<24) + (b_value << 16) + (g_value << 8) + (r_value << 0);//打包
-
-    xTaskNotify(xTaskNeoRing, rgb, eSetValueWithOverwrite);//覆盖
-
-    vTaskDelay(100);
-  }
-}
-void neoRing(void *pvParam)
-{
-  Adafruit_NeoPixel pixels(NUMPIXELS, NEOPIN, NEO_GRB + NEO_KHZ800);
-  pixels.begin();
-
-  uint32_t srgb = 0;
-  uint8_t sr = 0;
-  uint8_t sg = 0;
-  uint8_t sb = 0;
-  uint8_t ss = 100;
-
-  while(1)
-  {
-    pixels.clear();
-    for(int i = 0;i < NUMPIXELS; i++)
+    if(ulNotificationValue > 0)
     {
-      xTaskNotifyWait(0x00, 0x00, &srgb, 0);
-      ss = (srgb & 0xff000000) >> 24;//解包
-      sg = (srgb & 0x00ff0000) >> 16;
-      sb = (srgb & 0x0000ff00) >> 8;
-      sr = (srgb & 0x000000ff) >> 0;
-
-      pixels.setPixelColor(i, pixels.Color(sr, sg, sb));
-      pixels.show();
-
-      vTaskDelay(ss*5);
+      digitalWrite(23, !digitalRead(23));
+      vTaskDelay(1000);
     }
   }
 }
+
+void ReadBth(void *pvParam)
+{
+  pinMode(22, INPUT_PULLUP);
+  while(1)
+  {
+    if(digitalRead(22) == LOW) {
+      xTaskNotifyGive(xflashLED);//相当于xTaskNotify()+eIncrement//也就是xSemaphoreGive
+      vTaskDelay(120);
+    }
+  }
+}
+
