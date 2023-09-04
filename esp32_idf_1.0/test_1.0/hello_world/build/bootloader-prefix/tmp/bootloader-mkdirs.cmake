0474bbc8bb4f228f0337369b38923379d2085d86
octopuslearn <120025758+octopuslearn@users.noreply.github.com>
2023-09-04 20:23:39 +0800

    分了区
    partitions_two.csv文件
    
    # Name,   Type, SubType, Offset,  Size, Flags
    # Note: if you have increased the bootloader size, make sure to update the offsets to avoid overlap
    nvs,      data, nvs,     ,        0x4000,
    otadata,  data, ota,     ,        0x2000,
    phy_init, data, phy,     ,        0x1000,
    factory,  app,  factory, ,        1M,
    ota_0,    app,  ota_0,   ,        1M,
    ota_1,    app,  ota_1,   ,        1M,

---
+++ esp32_idf_1.0/test_1.0/hello_world/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "D:/Installation_app/Espressif/frameworks/esp-idf-v5.1.1/components/bootloader/subproject"
+  "E:/project/ESP32_learn/esp32_idf_1.0/test_1.0/hello_world/build/bootloader"
+  "E:/project/ESP32_learn/esp32_idf_1.0/test_1.0/hello_world/build/bootloader-prefix"
+  "E:/project/ESP32_learn/esp32_idf_1.0/test_1.0/hello_world/build/bootloader-prefix/tmp"
+  "E:/project/ESP32_learn/esp32_idf_1.0/test_1.0/hello_world/build/bootloader-prefix/src/bootloader-stamp"
+  "E:/project/ESP32_learn/esp32_idf_1.0/test_1.0/hello_world/build/bootloader-prefix/src"
+  "E:/project/ESP32_learn/esp32_idf_1.0/test_1.0/hello_world/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "E:/project/ESP32_learn/esp32_idf_1.0/test_1.0/hello_world/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "E:/project/ESP32_learn/esp32_idf_1.0/test_1.0/hello_world/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
